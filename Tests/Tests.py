a84a34eff63bb513a03d6f151903ce6858039f71
SkyLined <github@skylined.nl>
2020-06-17 11:41:08 +0200

    Add tests (rudimentery)

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,2 +1,9 @@
-# I should add some tests here.
-pass;
+from fTestDependencies import fTestDependencies;
+fTestDependencies();
+
+from mDebugOutput import fEnableDebugOutputForClass, fEnableDebugOutputForModule, fTerminateWithException;
+try:
+  # Tests are yet to be added.
+  pass;
+except Exception as oException:
+  fTerminateWithException(oException);
