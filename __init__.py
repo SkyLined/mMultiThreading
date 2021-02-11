3a10b762ae950ce5f1e46cbf1e03560f72e66871
SkyLined <github@skylined.nl>
2021-02-11 13:19:03 +0100

    Move __init__ to mMultiThreading

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
-from cLock import cLock;
-from cThread import cThread;
-from cWithCallbacks import cWithCallbacks;
-
-__all__ = [
-  "cLock",
-  "cThread",
-  "cWithCallbacks",
-];
+from .mMultiThreading import *;
